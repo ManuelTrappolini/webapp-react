d246001a389685b9913db11e136ba018a393cf45
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-12-16 12:02:48 +0100

    import ldrs , add globalcontext and loader component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/Home"
 import Layout from "./pages/Layout"
 import SingleMovie from "./pages/SingleMovie"
-
+import GlobalContext from "./context/GlobalContext"
+import { useState } from "react"
 
 
 function App() {
 
+  const { loading, setLoading } = useState(false)
+  const values = {
+    loading,
+    setLoading
+  }
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />} >
-            <Route path='/' element={<HomePage />} />
-            <Route path='movies/:id' element={<SingleMovie />} />
-          </Route>
+      <GlobalContext.Provider value={values}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />} >
+              <Route path='/' element={<HomePage />} />
+              <Route path='movies/:id' element={<SingleMovie />} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import 'ldrs/ring'
+
+
+export default function Loader() {
+    return (
+        <>
+            <div>
+                <l-ring size="40" stroke="5" bg-opacity="0" speed="2" color="black"></l-ring>
+            </div>
+        </>
+    )
+}
